f912605f27e927ba2d3ca3632f4c007ab11801c1
bagusprasetyow <baguspw17@gmail.com>
2022-11-27 19:36:18 +0700

    #TASK_8

--- latihan.php
+++ latihan.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Latihan Looping</title>
-</head>
-<body>
-    <h2>Membuat Perulangan Tabel</h2>
-    <table border="1" cellpadding="10" cellspacing="0">
-        <?php
-       for ($b = 1; $b <= 10; $b++) {
-            echo "<tr>";
-            for ($k = 1; $k <= 8; $k++) {
-                echo "<td> $b,$k </td>";
-            }
-            echo "</tr>";
-        }
-        ?>
-    </table>
-</body>
-</html>
+<?php
+
+$x = 10;
+if ($x < 20){
+bracket
+    echo"benar";
+} else {
+    echo"salah";
+}
+
+?>
